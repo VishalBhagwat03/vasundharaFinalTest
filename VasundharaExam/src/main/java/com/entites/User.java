8c3ac9b72125cce7f202c32aeffdffe2a9cbb894
Vishal Bhagwat <83702624+VishalBhagwat03@users.noreply.github.com>
2023-05-05 16:23:15 +0530

    Update User.java

--- VasundharaExam/src/main/java/com/entites/User.java
+++ VasundharaExam/src/main/java/com/entites/User.java
@@ -26,14 +26,7 @@ public class User {
     UserId = userId;
     this.password = password;
   }
-  @Override
-  public int hashCode() {
-    final int prime = 31;
-    int result = 1;
-    result = prime * result + ((UserId == null) ? 0 : UserId.hashCode());
-    result = prime * result + ((password == null) ? 0 : password.hashCode());
-    return result;
-  }
+
   @Override
   public String toString() {
     return "User [UserId=" + UserId + ", password=" + password + "]";
